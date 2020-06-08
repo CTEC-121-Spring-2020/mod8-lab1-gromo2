a921c56afbcbf51c7f088fde52a5f502fa31e81b
Garrett <g.romo2@students.clark.edu>
2020-06-08 12:28:15 -0700

    Completed Lab

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+<Garrett>
+<Mod 8 Lab 1>
 <assignment/lab description
 """
 
@@ -12,6 +12,72 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    # Mod 7 assignment review
+    '''
+    myDict = {}
+    # key - value
+    # create element 
+    myDict["key"] = "value"
+    myDict["key"] = "new value"
+    print(myDict.keys() )
+    print(myDict.values() )
+    print(myDict.items() )
+    '''
+    '''
+    fileHandle = open("sample.txt", 'a')
+    print()
+    #print(fileHandle)
+    readReturn = fileHandle.read()
+    print(type(readReturn))
+    print("*",fileHandle.read(),"*" )
+    fileHandle.close()
+    print()
+    '''
+    '''
+    print()
+    print(5*'All that ')
+    print()
+    '''
+    # section 1
+    '''
+    win = GraphWin("demo", 1000, 1000)
+    myCircle = Circle(Point(500,500), 200)
+    myCircle.setFill("green")
+    myCircle.draw(win)
+    win.getMouse()
+    '''
+    # section 2
+    """
+    d = Dog("Dog")
+    print(d)
+    print(d.getName())
+    d.setName("Godzilla")
+    print(d.getName())
+    d.bark()
+    print()
+    # section 3
+    die = MSDie(12)
+    print("numer of sides", die.getSides())
+    print("value:", die.getValue())
+    die.setValue(5)
+    print("value:", die.getValue())
+    die.setValue(12)
+    print("value:", die.getValue())
+    die.roll()
+    print("value:", die.getValue())
+    die.roll()
+    print("value:", die.getValue())
+    die.roll()
+    print("value:", die.getValue())
+    """
+    print()
+    # print module/file header documention
+    print(docTest.__doc__)
+    # print class docs
+    print(docTest.DocTest.__doc__)
+    # print method docs
+    print(docTest.DocTest.method.__doc__)
+    print()
+
 
 main()    
